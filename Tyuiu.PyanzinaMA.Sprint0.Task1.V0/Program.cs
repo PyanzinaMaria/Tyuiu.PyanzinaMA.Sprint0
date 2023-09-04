5f9b64f3a27f5fc35efa1fbd579943462ce63a09
Tyuiu.PyanzinaMA@outlook.com <mrpnzn@yaandex.ru>
2023-09-04 21:36:54 +0500

    Task1

---
+++ Tyuiu.PyanzinaMA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.PyanzinaMA.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Маша";
+            int age = 19;
+            Console.WriteLine("Здравствуйте, меня зовут " + name +". Мне " + age + " лет:)"); 
+            Console.ReadKey();
+        }
+    }
+}
