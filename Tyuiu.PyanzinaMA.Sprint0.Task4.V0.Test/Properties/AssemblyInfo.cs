f890fea98542843cc6e66f19344a77b4137860d1
Tyuiu.PyanzinaMA@outlook.com <mrpnzn@yaandex.ru>
2023-09-08 20:25:13 +0500

    Task4

---
+++ Tyuiu.PyanzinaMA.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PyanzinaMA.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PyanzinaMA.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fc211c6d-c4fa-4571-b906-1bd7aa5e1dba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PyanzinaMA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.PyanzinaMA.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.PyanzinaMA.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(17, 5));
+            Console.WriteLine(DataService.Substraction(12, 8));
+            Console.WriteLine(DataService.Multiplication(3, 9));
+            Console.WriteLine(DataService.Division(10, 5));
+            Console.ReadKey();
+        }
+    }
+}
