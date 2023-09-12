bc7f5444b25670f1dfe7bfa54b2e5a05c39f4c54
Tyuiu.PyanzinaMA@outlook.com <mrpnzn@yaandex.ru>
2023-09-12 17:02:56 +0500

    Task5

---
+++ Tyuiu.PyanzinaMA.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PyanzinaMA.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PyanzinaMA.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("94753e5f-3842-45bf-8c6d-d49001ddc89b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PyanzinaMA.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.PyanzinaMA.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.PyanzinaMA.Sprint0.Task5.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = " + DataService.Addition(2, 7));
+            Console.WriteLine("A - B = " + DataService.Substraction(32, 24));
+            Console.WriteLine("A * B = " + DataService.Multiplication(7, 7));
+            Console.WriteLine("A / B = " + DataService.Division(27, 9));
+            Console.ReadKey();
+        }
+    }
+}
